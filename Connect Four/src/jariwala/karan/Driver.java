d4eb4c34f3a3336a80650ec691414112b69a0fc6
Karan <Bhavesh@PC.gateway.2wire.net>
2016-03-14 00:40:38 -0400

    Version 0.0.2

--- Connect Four/src/jariwala/karan/Driver.java
+++ Connect Four/src/jariwala/karan/Driver.java
@@ -1,10 +1,16 @@
 package jariwala.karan;
-
+import java.util.*;
 public class Driver {
 
 	public static void main(String[] args) {
-		// TEST
-
+		Scanner in = new Scanner (System.in);
+		System.out.println("  1   2   3   4   5   6   7");
+		System.out.println("A   |   |   |   |   |   |   |");
+		System.out.println("B   |   |   |   |   |   |   |");
+		System.out.println("C   |   |   |   |   |   |   |");
+		System.out.println("D   |   |   |   |   |   |   |");
+		System.out.println("E   |   |   |   |   |   |   |");
+		System.out.println("F   |   |   |   |   |   |   |");
 	}
 
 }
