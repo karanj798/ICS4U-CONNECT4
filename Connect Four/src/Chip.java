34b79f0c8a8c90fc61011d4ff9e588b812327d1f
kkaran360 <karanjari@live.com>
2016-03-21 01:12:48 -0400

    Create Chip.java

---
+++ Connect Four/src/Chip.java
@@ -0,0 +1,19 @@
+import java.awt.Color;
+
+public class Chip {
+	
+	private Color color;
+	
+	public Chip() {
+		this.color = Color.WHITE;
+	}
+
+	public Color getColor() {
+		return color;
+	}
+
+	public void setColor(Color color) {
+		this.color = color;
+	}
+
+}
