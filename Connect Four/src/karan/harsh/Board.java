b486f8a7c82b91ea058a76de8c71c7b26fd42c79
Karan <Bhavesh@PC.gateway.2wire.net>
2016-03-21 17:15:16 -0400

    Version 1.1

--- Connect Four/src/karan/harsh/Board.java
+++ Connect Four/src/karan/harsh/Board.java
@@ -7,7 +7,6 @@ public class Board {
 	boolean playerSwitch, gameEnd, playerAWon, playerBWon;
 	Chip chip = new Chip();
 
-
 	public Board(int x, int y) {
 
 	}
@@ -68,71 +67,87 @@ public class Board {
 	public void checkWinnerA() {
 		for (int i = 0; i < 7; i++) {
 			for (int j = 0; j < 7; j++) {
-				if (i >= 3) {					
-					if (board[i][j] == 'Y' && board[i-1][j] == 'Y' && board [i-2][j] == 'Y' && board [i-3][j] == 'Y'){
-						setGameOver (true, false);
-					}		
-				}
-				if (j <= 3){
-					if (board[i][j] == 'Y' && board[i][j+1] == 'Y' && board[i][j+2] == 'Y' && board[i][j+3] == 'Y'){
+				if (i >= 3) {
+					if (board[i][j] == 'Y' && board[i - 1][j] == 'Y' && board[i - 2][j] == 'Y'
+							&& board[i - 3][j] == 'Y') {
+						setWinnerA(true);
 						setGameOver(true, false);
 					}
 				}
-				if (i <= 3 && j <= 3){
-					if (board[i][j] == 'Y' && board[i+1][j+1] == 'Y' && board[i+2][j+2] == 'Y' && board[i+3][j+3] == 'Y'){
+				if (j <= 3) {
+					if (board[i][j] == 'Y' && board[i][j + 1] == 'Y' && board[i][j + 2] == 'Y'
+							&& board[i][j + 3] == 'Y') {
+						setWinnerA(true);
 						setGameOver(true, false);
 					}
 				}
-				if ( i <= 3 && j >= 3) {
-					if (board[i][j] == 'Y' && board[i+1][j-1] == 'Y' && board[i+2][j-2] == 'Y' && board[i+3][j-3] == 'Y'){
+				if (i <= 3 && j <= 3) {
+					if (board[i][j] == 'Y' && board[i + 1][j + 1] == 'Y' && board[i + 2][j + 2] == 'Y'
+							&& board[i + 3][j + 3] == 'Y') {
+						setWinnerA(true);
+						setGameOver(true, false);
+					}
+				}
+				if (i <= 3 && j >= 3) {
+					if (board[i][j] == 'Y' && board[i + 1][j - 1] == 'Y' && board[i + 2][j - 2] == 'Y'
+							&& board[i + 3][j - 3] == 'Y') {
+						setWinnerA(true);
 						setGameOver(true, false);
 					}
 				}
 			}
 		}
 	}
-	
-	public void setWinnerA (boolean condition){
-		this.playerAWon = condition;
-	}
-	
-	public void setWinnerB (boolean condition){
-		this.playerBWon = condition;
-	}
-	
-	public boolean getWinnerA (){
-		return playerAWon;
-	}
-	
-	public boolean getWinnerB(){
-		return playerBWon;
-	}
-	
+
 	public void checkWinnerB() {
 		for (int i = 0; i < 7; i++) {
 			for (int j = 0; j < 7; j++) {
-				if (i >= 3) {					
-					if (board[i][j] == 'R' && board[i-1][j] == 'R' && board [i-2][j] == 'R' && board [i-3][j] == 'R'){
-						setGameOver (false, true);
-					}		
-				}
-				if (j <= 3){
-					if (board[i][j] == 'R' && board[i][j+1] == 'R' && board[i][j+2] == 'R' && board[i][j+3] == 'R'){
+				if (i >= 3) {
+					if (board[i][j] == 'R' && board[i - 1][j] == 'R' && board[i - 2][j] == 'R'
+							&& board[i - 3][j] == 'R') {
+						setWinnerB(true);
 						setGameOver(false, true);
 					}
 				}
-				if (i <= 3 && j <= 3){
-					if (board[i][j] == 'R' && board[i+1][j+1] == 'R' && board[i+2][j+2] == 'R' && board[i+3][j+3] == 'R'){
+				if (j <= 3) {
+					if (board[i][j] == 'R' && board[i][j + 1] == 'R' && board[i][j + 2] == 'R'
+							&& board[i][j + 3] == 'R') {
+						setWinnerB(true);
 						setGameOver(false, true);
 					}
 				}
-				if ( i <= 3 && j >= 3) {
-					if (board[i][j] == 'R' && board[i+1][j-1] == 'R' && board[i+2][j-2] == 'R' && board[i+3][j-3] == 'R'){
+				if (i <= 3 && j <= 3) {
+					if (board[i][j] == 'R' && board[i + 1][j + 1] == 'R' && board[i + 2][j + 2] == 'R'
+							&& board[i + 3][j + 3] == 'R') {
+						setWinnerB(true);
+						setGameOver(false, true);
+					}
+				}
+				if (i <= 3 && j >= 3) {
+					if (board[i][j] == 'R' && board[i + 1][j - 1] == 'R' && board[i + 2][j - 2] == 'R'
+							&& board[i + 3][j - 3] == 'R') {
+						setWinnerB(true);
 						setGameOver(false, true);
 					}
 				}
 			}
 		}
+	}
+
+	public void setWinnerA(boolean condition) {
+		this.playerAWon = condition;
+	}
+
+	public void setWinnerB(boolean condition) {
+		this.playerBWon = condition;
+	}
+
+	public boolean getWinnerA() {
+		return playerAWon;
+	}
+
+	public boolean getWinnerB() {
+		return playerBWon;
 	}
 
 	public void setGameOver(boolean a, boolean b) {
